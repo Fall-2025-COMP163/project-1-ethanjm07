6ce052d942720a32f8fb1218a8813e18161783ce
ethanjm07 <ethanjm07@icloud.com>
2025-10-24 08:52:11 -0400

    Character creation on stat calculation

--- project1_starter.py
+++ project1_starter.py
@@ -1,7 +1,7 @@
 """
 COMP 163 - Project 1: Character Creator & Saving/Loading
-Name: [Your Name Here]
-Date: [Date]
+Name: Ethan Mitchell
+Date: 10/20/25
 
 AI Usage: [Document any AI assistance used]
 Example: AI helped with file I/O error handling logic in save_character function
@@ -11,11 +11,25 @@ def create_character(name, character_class):
     """
     Creates a new character dictionary with calculated stats
     Returns: dictionary with keys: name, class, level, strength, magic, health, gold
+    """
+    level = 1
+    strength, magic, health = calculate_stats(character_class, level)
+    char = {'name' : name,
+            'class': character_class,
+            'level': level,
+            'strength':strength,
+            'magic':magic,
+            'health':health,
+            'gold': 50}
+    return char
     
+    """  
     Example:
     char = create_character("Aria", "Mage")
     # Should return: {"name": "Aria", "class": "Mage", "level": 1, "strength": 5, "magic": 15, "health": 80, "gold": 100}
     """
+    
+    
     # TODO: Implement this function
     # Remember to use calculate_stats() function for stat calculation
     pass
@@ -24,7 +38,30 @@ def calculate_stats(character_class, level):
     """
     Calculates base stats based on class and level
     Returns: tuple of (strength, magic, health)
+    """
+    base_strength = 5
+    base_magic = 5
+    base_health = 10
     
+    if character_class == "Warrior":
+        strength = (base_strength * 5)*level
+        magic = (base_magic * 1)*level
+        health = (base_health * 5)*level
+    elif character_class == "Mage":
+        strength = (base_strength * 1) * level
+        magic = (base_magic * 5) * level
+        health = (base_health * 3) * level
+    elif character_class == "Rogue":
+        strength = (base_strength * 3) * level
+        magic = (base_magic * 3) * level
+        health = (base_health * 1) * level
+    elif character_class == "Cleric":
+        strength = (base_strength * 3) * level
+        magic = (base_magic * 3) * level
+        health = (base_health * 1) * level
+    stats = (strength,magic,health,)
+    return stats
+    """
     Design your own formulas! Ideas:
     - Warriors: High strength, low magic, high health
     - Mages: Low strength, high magic, medium health  

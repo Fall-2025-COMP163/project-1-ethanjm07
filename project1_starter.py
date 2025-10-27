b7c9a5c02c22a8e3b0020b04ab9e03def62c0db4
ethanjm07 <ethanjm07@icloud.com>
2025-10-27 16:38:22 -0400

    Display Character Sheet

--- project1_starter.py
+++ project1_starter.py
@@ -95,17 +95,28 @@ def load_character(filename):
     Loads character from text file
     Returns: character dictionary if successful, None if file not found
     """
-    
+    with open(filename,'r') as file:
+        contents = file.read(filename)
+        return contents
         
     # TODO: Implement this function
     # Remember to handle file not found errors
-    pass
+    
 
 def display_character(character):
     """
     Prints formatted character sheet
     Returns: None (prints to console)
     """""
+    print("=== CHARACTER SHEET ===")
+    print(f"Name: {character['name']}")
+    print(f"Class: {character['class']}")
+    print(f"Level: {character['level']}")
+    print(f"Strength: {character['strength']}")
+    print(f"Magic: {character['magic']}")
+    print(f"Health: {character['health']}")
+    print(f"Gold: {character['gold']}")
+    
     """""    Example output:
     === CHARACTER SHEET ===
     Name: Aria
@@ -117,7 +128,7 @@ def display_character(character):
     Gold: 100
     """
     # TODO: Implement this function
-    pass
+    
 
 def level_up(character):
     """
